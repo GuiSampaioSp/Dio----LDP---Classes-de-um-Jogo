1e3d856a65c261bf0d7f0d872bdc5d3c5f7b2429
GuiSampaioSp <city.ermelino@gmail.com>
2025-02-13 11:33:47 -0300

    Add files via upload

---
+++ Desafio - Classes de Jogo (minha versão).js
@@ -0,0 +1,13 @@
+let classe = "Ninja"
+let ataque = ""
+
+if (classe === "Mago")
+    {ataque = "Magia"}
+else if (classe === "Guerreiro")
+    {ataque = "Espada"}
+else if (classe === "Monge")
+    {ataque = "Artes Marciais"}
+else if (classe === "Ninja")
+    {ataque = "Shuriken"}
+
+console.log ("O " + classe + " atacou usando " + ataque)
